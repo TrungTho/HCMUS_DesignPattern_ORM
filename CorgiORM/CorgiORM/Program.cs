79ebe6586859158588766200b65e7e860805295d
TrungTho <41388666+TrungTho@users.noreply.github.com>
2020-12-05 18:10:30 +0700

    init

---
+++ CorgiORM/CorgiORM/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+using System.Data.OleDb;
+
+namespace CorgiORM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //muon ket noi db thi cu copy 2 dong using cuoi qua file can dung
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
