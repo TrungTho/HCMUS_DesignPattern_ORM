b8fec18d18842c417620a5fb09b2cd20dc2f313c
TrungTho <41388666+TrungTho@users.noreply.github.com>
2020-12-21 19:31:44 +0700

    test basis connect & query by ado.net

--- CorgiORM/CorgiORM/Program.cs
+++ CorgiORM/CorgiORM/Program.cs
@@ -6,16 +6,74 @@ using System.Threading.Tasks;
 
 using System.Data.SqlClient;
 using System.Data.OleDb;
+using System.Diagnostics;
 
 namespace CorgiORM
 {
     class Program
     {
+        /// <summary>
+        /// Entity class - Contains data only
+        /// 
+        /// </summary>
+        class Customer
+        {
+            public int ID { get; set; }
+            public string Fullname { get; set; }
+            public string Tel { get; set; }
+
+            public override string ToString()
+            {
+                return $"{ID} - {Fullname} - {Tel}";
+            }
+        }
+
         static void Main(string[] args)
         {
-            //muon ket noi db thi cu copy 2 dong using cuoi qua file can dung
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+
+            // Ket qua cua viec doc du lieu
+            var listOfCustomer = new List<Customer>();
+
+            // Buoc 1: Mo ket noi
+            var connectionString = "Server=localhost\\SqlExpress;Database=MyCompany; Trusted_connection=yes";
+            var connection = new SqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+
+                // Buoc 2 - Chuan bi cau truy van
+                // Lay danh sach cac customer
+                var sql = "SELECT * FROM Customer";
+
+                // Buoc 3 - Thuc thi cau truy van
+                var command = new SqlCommand(sql, connection);
+                var reader = command.ExecuteReader();
+
+                // Thuc hien anh xa
+                while (reader.Read())
+                {
+                    var customer = new Customer();
+                    customer.ID = (int)reader[0];
+                    customer.Fullname = (string)reader[1];
+                    customer.Tel = (string)reader[2];
+                    listOfCustomer.Add(customer);
+                }
+
+                foreach (var x in listOfCustomer)
+                    Console.WriteLine(x.ToString());
+
+                Debug.WriteLine("Successful!!!");
+
+                // Buoc 5: Dong ket noi
+                connection.Close();
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+
+                Debug.WriteLine(e);
+            }
         }
+
     }
 }
