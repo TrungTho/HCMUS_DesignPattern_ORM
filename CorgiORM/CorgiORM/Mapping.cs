0cdfcae049e40b30d93ea1d82fa8c37ce9d80987
VinhNguyen <nhvinh604@gmail.com>
2020-12-21 21:20:27 +0700

    add class a connect DB method

--- CorgiORM/CorgiORM/Mapping.cs
+++ CorgiORM/CorgiORM/Mapping.cs
@@ -4,10 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CorgiORM
-{
-    class Mapping
-    {
+using System.Data.SqlClient;
+using System.Data.OleDb;
+
+namespace CorgiORM {
+    class Mapping {
+
+        public Mapping() {
+            ConnectDB();
+        }
+
+        public void ConnectDB() {
+            string connectionString;
+            OleDbConnection cnn;
+
+            connectionString = "Server=localhost\\SqlExpress;" +
+                "Database=MyCompany;" +
+                "Trusted_Connection=yes;" +
+                "Provider=SQLOLEDB";
+            cnn = new OleDbConnection(connectionString);
+            Console.WriteLine(connectionString);
+            cnn.Open();
+            Console.WriteLine("Connected");
+        }
 
     }
 }
--- CorgiORM/CorgiORM/Program.cs
+++ CorgiORM/CorgiORM/Program.cs
@@ -7,15 +7,16 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data.OleDb;
 
-namespace CorgiORM
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //muon ket noi db thi cu copy 2 dong using cuoi qua file can dung
+namespace CorgiORM {
+    class Program {
+        static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+
+            Mapping map = new Mapping();
+
+
             Console.ReadKey();
+
         }
     }
 }
