fb9a0922b0b0ede0cd600160151c8bbc5eb4482f
TrungTho <41388666+TrungTho@users.noreply.github.com>
2021-01-09 01:31:20 +0700

    testing with ado parameters

--- CorgiORM/CorgiORM/Program.cs
+++ CorgiORM/CorgiORM/Program.cs
@@ -39,34 +39,50 @@ namespace CorgiORM
             var connection = new SqlConnection(connectionString);
             try
             {
-                connection.Open();
-
-                // Buoc 2 - Chuan bi cau truy van
-                // Lay danh sach cac customer
-                var sql = "SELECT * FROM Customer";
-
-                // Buoc 3 - Thuc thi cau truy van
-                var command = new SqlCommand(sql, connection);
-                var reader = command.ExecuteReader();
-
-                // Thuc hien anh xa
-                while (reader.Read())
+                while (true)
                 {
-                    var customer = new Customer();
-                    customer.ID = (int)reader[0];
-                    customer.Fullname = (string)reader[1];
-                    customer.Tel = (string)reader[2];
-                    listOfCustomer.Add(customer);
+                    connection.Open();
+
+                    int count = 0;
+
+
+                    // Buoc 2 - Chuan bi cau truy van
+                    // Lay danh sach cac customer
+                    var sql = "SELECT * FROM Customer where ID=@id";
+
+
+                    // Buoc 3 - Thuc thi cau truy van
+                    var command = new SqlCommand(sql, connection);
+
+                    //parameter
+                    command.Parameters.Add("@id", OleDbType.Integer).Value=1;
+                    //command.Parameters["@id"].Value = 1;
+
+                    var reader = command.ExecuteReader();
+
+                    // Thuc hien anh xa
+                    while (reader.Read())
+                    {
+                        var customer = new Customer();
+                        customer.ID = (int)reader[0];
+                        customer.Fullname = (string)reader[1];
+                        customer.Tel = (string)reader[2];
+                        listOfCustomer.Add(customer);
+                    }
+
+                    foreach (var x in listOfCustomer)
+                        Console.WriteLine(x.ToString());
+                    Console.WriteLine($"--------{count++}----------");
+
+
+                    Debug.WriteLine($"Successful!!");
+
+                    Console.ReadKey();
+
+                    // Buoc 5: Dong ket noi
+                    connection.Close();
+                    Console.ReadKey();
                 }
-
-                foreach (var x in listOfCustomer)
-                    Console.WriteLine(x.ToString());
-
-                Debug.WriteLine("Successful!!!");
-
-                // Buoc 5: Dong ket noi
-                connection.Close();
-                Console.ReadKey();
             }
             catch (Exception e)
             {
