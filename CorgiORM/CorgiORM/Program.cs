f174fa9bd9f5daab10366503f8d1f6c29630d7b5
TrungTho <41388666+TrungTho@users.noreply.github.com>
2021-01-11 01:43:14 +0700

    get field name with dataTable.Columns[index]

--- CorgiORM/CorgiORM/Program.cs
+++ CorgiORM/CorgiORM/Program.cs
@@ -30,23 +30,30 @@ namespace CorgiORM
                 this.Fullname= name;
                 this.Tel = tel;
             }
+
+            public Customer(string name, string tel)
+            {
+                this.Fullname = name;
+                this.Tel = tel;
+            }
         }
 
-        [STAThread]
+        static public string connectionString = "Server=localhost\\SqlExpress;Database=MyCompany; Trusted_connection=yes";
         static void Main(string[] args)
         {
 
             // Ket qua cua viec doc du lieu
             var listOfCustomer = new List<Customer>();
+            string tableName = null;
 
             // Buoc 1: Mo ket noi
-            var connectionString = "Server=localhost\\SqlExpress;Database=MyCompany; Trusted_connection=yes";
             var connection = new SqlConnection(connectionString);
-            while (false)
+            bool kt = true;
+            while (kt)
             {
                 try
                 {
-                    var tableName = "customer";
+                    tableName = "customer";
                     string queryStr = $"select * from {tableName}";
 
                     var adapter = new SqlDataAdapter(queryStr, connection);
@@ -54,9 +61,10 @@ namespace CorgiORM
 
                     var data = new DataSet();
 
-                    adapter.Fill(data, tableName);
+                    kt = false;
 
-                    //Console.WriteLine(data.Tables.);
+                    adapter.Fill(data, tableName);
+                    DataTable dataTable = data.Tables[tableName];
 
                     List<Customer> listCustomer = new List<Customer>();
 
@@ -74,10 +82,48 @@ namespace CorgiORM
                     }
 
                     Console.WriteLine("------------------------");
+                    var fieldName = dataTable.Columns;
+                    Console.WriteLine($"{fieldName[0]}--{fieldName[1]}--{fieldName[2]}");
                     foreach (var datum in listCustomer)
                         Console.WriteLine(datum.ToString());
 
+                    ///////////////////////////////////////////////////
+                    //queryStr = $"update {tableName} set tel='0' where id =10";
+                    //adapter = new SqlDataAdapter(queryStr, connection);
+                    //builder = new SqlCommandBuilder(adapter);
+
+                    //data = new DataSet();
+
+                    //kt = false;
+
+                    //adapter.Fill(data, tableName);
+
+                    //listCustomer = new List<Customer>();
+
+                    //for (var i = 0; i < data.Tables[tableName].Rows.Count; i++)
+                    //{
+                    //    var row = data.Tables[tableName].Rows[i];
+
+                    //    //mapping
+                    //    Customer customer = new Customer(
+                    //        (int)row[0],
+                    //        (string)row[1],
+                    //        (string)row[2]);
+
+                    //    listCustomer.Add(customer);
+                    //}
+
+                    //Console.WriteLine("------------------------");
+                    //foreach (var datum in listCustomer)
+                    //    Console.WriteLine(datum.ToString());
+
                     Console.ReadKey();
+
+                    //var obj = new Customer(12, "OK", "0.10.10.1");
+                    //foreach (PropertyInfo attri in obj.GetType().GetProperties())
+                    //{
+
+                    //}
 
                 }
                 catch (Exception e)
@@ -87,20 +133,26 @@ namespace CorgiORM
                 }
             }
 
-            var test = new Customer(1,"OK","0.10.10.1");
-            getAllValueInObject(test);
-            Console.ReadKey();
+            //var newobj = new Customer(12, "OK","0.10.10.1");
+            //CorgiInsert(newobj,tableName);
+            //Console.ReadKey();
         }
 
-        static void getAllValueInObject<T>(T myObj)
+        static void CorgiInsert<T>(T myObj,string tableName)
         {
             Console.WriteLine(myObj.GetType());
             Console.WriteLine("Parsing...");
             foreach (PropertyInfo attri in myObj.GetType().GetProperties())
             {
-                //var value = attri.GetValue() as 
-                Console.WriteLine(attri.GetValue(myObj,null));
+                var value = attri.GetValue(myObj, null);
+                bool checkString = false;
+                if (value.GetType() == typeof(string)) checkString = true;
+                Console.WriteLine($"{value} ------- {value.GetType()} ---- {checkString}");
             }
         }
+
+        
+
+
     }
 }
