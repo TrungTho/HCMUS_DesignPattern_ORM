eb3599cd9eed639bec065e49abea9b099f813667
TrungTho <41388666+TrungTho@users.noreply.github.com>
2021-01-11 20:04:07 +0700

    apply design patterns (basis testing code)

--- CorgiORM/CorgiORM/Program.cs
+++ CorgiORM/CorgiORM/Program.cs
@@ -9,150 +9,91 @@ namespace CorgiORM
 {
     class Program
     {
-        /// <summary>
-        /// Entity class - Contains data only
-        /// 
-        /// </summary>
-        class Customer
+        public class CorgiORM
         {
-            public int ID { get; set; }
-            public string Fullname { get; set; }
-            public string Tel { get; set; }
+            private string connectionString;
 
-            public override string ToString()
+            public string ConnectionString { get => connectionString; set => connectionString = value; }
+
+            public IExecute CorgiAdd;
+            public IExecute CorgiUpdate;
+
+            public CorgiORM(string connectionString)
             {
-                return $"{ID} - {Fullname} - {Tel}";
+                this.connectionString = connectionString;
+                getExecuteType();
             }
 
-            public Customer(int id, string name, string tel)
+            private void getExecuteType()
             {
-                this.ID = id;
-                this.Fullname= name;
-                this.Tel = tel;
-            }
-
-            public Customer(string name, string tel)
-            {
-                this.Fullname = name;
-                this.Tel = tel;
+                this.CorgiAdd = new SqlAdd(this.connectionString);
+                this.CorgiUpdate = new SqlUpdate(this.connectionString);
             }
         }
 
-        static public string connectionString = "Server=localhost\\SqlExpress;Database=MyCompany; Trusted_connection=yes";
+        public interface IExecute
+        {
+            void execute<T>(string tableName, T Object);
+        }
+
+        public class SQLExecute : IExecute
+        {
+            protected string connectionString;
+            protected string tableName;
+            protected string queryString;
+
+            public SQLExecute(string connectionString)
+            {
+                this.connectionString = connectionString;
+            }
+
+            protected virtual string buildQuery<T>(string tableName, T Object)
+            {
+                return null;
+            }
+
+
+            public void execute<T>(string tableName, T Object)
+            {
+                this.queryString = buildQuery<T>(tableName, Object);
+                Console.WriteLine($"{queryString} -- {tableName}");
+            }
+
+
+        }
+
+        public class SqlAdd : SQLExecute
+        {
+            public SqlAdd(string connectionString) : base(connectionString)
+            {
+
+            }
+
+            protected override string buildQuery<T>(string tableName, T Object)
+            {
+                return "Add";
+            }
+        }
+
+        public class SqlUpdate : SQLExecute
+        {
+            public SqlUpdate(string connectionString) : base(connectionString)
+            {
+
+            }
+
+            protected override string buildQuery<T>(string tableName, T Object)
+            {
+                return "SqlUpdate";
+            }
+        }
+
         static void Main(string[] args)
         {
-
-            // Ket qua cua viec doc du lieu
-            var listOfCustomer = new List<Customer>();
-            string tableName = null;
-
-            // Buoc 1: Mo ket noi
-            var connection = new SqlConnection(connectionString);
-            bool kt = true;
-            while (kt)
-            {
-                try
-                {
-                    tableName = "customer";
-                    string queryStr = $"select * from {tableName}";
-
-                    var adapter = new SqlDataAdapter(queryStr, connection);
-                    var builder = new SqlCommandBuilder(adapter);
-
-                    var data = new DataSet();
-
-                    kt = false;
-
-                    DataTable dataTable = data.Tables[tableName];
-                    adapter.Fill(data, tableName);
-
-                    List<Customer> listCustomer = new List<Customer>();
-
-                    for (var i = 0; i < data.Tables[tableName].Rows.Count; i++)
-                    {
-                        var row = data.Tables[tableName].Rows[i];
-
-                        //mapping
-                        Customer customer = new Customer(
-                            (int)row[0],
-                            (string)row[1],
-                            (string)row[2]);
-
-                        listCustomer.Add(customer);
-                    }
-
-                    Console.WriteLine("------------------------");
-                    var fieldName = dataTable.Columns;
-                    Console.WriteLine($"{fieldName[0]}--{fieldName[1]}--{fieldName[2]}");
-                    foreach (var datum in listCustomer)
-                        Console.WriteLine(datum.ToString());
-
-                    ///////////////////////////////////////////////////
-                    //queryStr = $"update {tableName} set tel='0' where id =10";
-                    //adapter = new SqlDataAdapter(queryStr, connection);
-                    //builder = new SqlCommandBuilder(adapter);
-
-                    //data = new DataSet();
-
-                    //kt = false;
-
-                    //adapter.Fill(data, tableName);
-
-                    //listCustomer = new List<Customer>();
-
-                    //for (var i = 0; i < data.Tables[tableName].Rows.Count; i++)
-                    //{
-                    //    var row = data.Tables[tableName].Rows[i];
-
-                    //    //mapping
-                    //    Customer customer = new Customer(
-                    //        (int)row[0],
-                    //        (string)row[1],
-                    //        (string)row[2]);
-
-                    //    listCustomer.Add(customer);
-                    //}
-
-                    //Console.WriteLine("------------------------");
-                    //foreach (var datum in listCustomer)
-                    //    Console.WriteLine(datum.ToString());
-
-                    Console.ReadKey();
-
-                    //var obj = new Customer(12, "OK", "0.10.10.1");
-                    //foreach (PropertyInfo attri in obj.GetType().GetProperties())
-                    //{
-
-                    //}
-
-                }
-                catch (Exception e)
-                {
-
-                    Debug.WriteLine(e);
-                }
-            }
-
-            //var newobj = new Customer(12, "OK","0.10.10.1");
-            //CorgiInsert(newobj,tableName);
-            //Console.ReadKey();
+            CorgiORM corgiORM = new CorgiORM("dbconnectionstring");
+            corgiORM.CorgiAdd.execute("user", 1);
+            corgiORM.CorgiUpdate.execute("customer", 2);
+            Console.ReadKey();
         }
-
-        static void CorgiInsert<T>(T myObj,string tableName)
-        {
-            Console.WriteLine(myObj.GetType());
-            Console.WriteLine("Parsing...");
-            foreach (PropertyInfo attri in myObj.GetType().GetProperties())
-            {
-                var value = attri.GetValue(myObj, null);
-                bool checkString = false;
-                if (value.GetType() == typeof(string)) checkString = true;
-                Console.WriteLine($"{value} ------- {value.GetType()} ---- {checkString}");
-            }
-        }
-
-        
-
-
     }
 }
